adc284bdf4b10055938823f74a3dfe22010a1dcb
whxaxes <whxaxes@gmail.com>
2022-12-21 18:20:10 +0800

    feat: first impl

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
-export * from './cmd/dev';
-export * from './cmd/debug';
+import { Program, CommandContext, Inject, ApplicationLifecycle, LifecycleHook, LifecycleHookUnit } from '@artus-cli/artus-cli';
+
+@LifecycleHookUnit()
+export default class VersionLifecycle implements ApplicationLifecycle {
+  @Inject()
+  private readonly program: Program;
+
+  @LifecycleHook()
+  async configDidLoad() {
+    const { rootCommand } = this.program;
+    this.program.option({
+      version: {
+        type: 'boolean',
+        alias: 'v',
+        description: 'Show Version',
+      },
+    }, [ rootCommand ]);
+
+    // intercept root command and show version
+    this.program.useInCommand(rootCommand, async (ctx: CommandContext, next) => {
+      const { args } = ctx;
+      if (args.version) {
+        return console.info(this.program.binName, this.program.version || '1.0.0');
+      }
+
+      await next();
+    });
+  }
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,17 @@
 import { run } from './test-utils';
 
 describe('test/index.test.ts', () => {
-  it('should --help', async () => {
-    await run('--help')
+  it('should --version', async () => {
+    await run('my-bin', '--version')
       // .debug()
-      .expect('stdout', /Available Commands/)
+      .expect('stdout', /my-bin 1.0.1/)
+      .end();
+  });
+
+  it('should -v', async () => {
+    await run('my-bin', '-v')
+      // .debug()
+      .expect('stdout', /my-bin 1.0.1/)
       .end();
   });
 });
